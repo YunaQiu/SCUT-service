40562a386695f67599594618ad08dee41dc4b8c5
qiuyu <qiuyu719@163.com>
2014-02-14 12:44:41 +0800

    完善图书详细信息
    ""

--- searchbook.js
+++ searchbook.js
@@ -3,7 +3,7 @@
 // trim string
 if (typeof(String.prototype.trim) !== "function") {
 	String.prototype.trim = function() {
-		return this.replace(/(^\s+)|(\s+$)/g, '');
+		return this.replace(/(^\s+)|(\s+$)/g, "");
 	}
 }
 
